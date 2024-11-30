8d67109035e1ee3f61b3c31b5dc147eb67d2e0aa
David Sandoval <davidsandoval281994@gmail.com>
2024-11-30 14:27:02 -0800

    added test statge and lambda stack

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super( scope, id, props );
     
+    const pipeline = new CodePipeline( this, 'Pipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep( 'Synth', {
+        input: CodePipelineSource.gitHub( 'jsandoval6/cdk-cicd', 'cicd-practice' ),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: './cdk.out'
+      })
+    } )
+    
+    new cdk.CfnOutput( this, 'PipelineName', {
+      value: 'AwesomePipeline'
+    } )
+
+    const testStage = pipeline.addStage( new PipelineStage( this, 'PipelineTestStage', {
+      stageName: 'Test'
+    }))
   }
 }
