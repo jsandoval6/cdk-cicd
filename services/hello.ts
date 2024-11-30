d6771f1a5ece341aeed9cc2e37acb07f81796502
David Sandoval <davidsandoval281994@gmail.com>
2024-11-30 14:50:00 -0800

    added hello lambda and esbuild

---
+++ services/hello.ts
@@ -0,0 +1,9 @@
+
+export async function handler ( event: any, context: any ) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello World!'
+        })
+    }
+}
